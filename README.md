effe29530a2d50ebad236ae33fc0b837859f07b3
ramersonw <ramersonw@hotmail.com>
2021-10-19 22:00:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dogs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dogs)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
