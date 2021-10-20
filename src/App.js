12de2b658b9b56cb21afbe562b34a90c3b328149
ramersonw <ramersonw@hotmail.com>
2021-10-20 13:07:47 -0300

    formatting

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import Home from './components/Home';
 import Login from './components/Login/Login';
 
 export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
